874dcf0a920636bd34fec34f9ca10de07a77674f
Lucas Montero <94916895+WorkdayLucas@users.noreply.github.com>
2022-07-22 17:59:20 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -14,7 +14,7 @@ app.use(morgan("dev"))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'https://app-chat-chat-8i7bmqsls-workdaylucas.vercel.app/',
+    origin: 'https://app-chat-chat-8i7bmqsls-workdaylucas.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'authorization'],
