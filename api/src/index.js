1a94513d5c4388776c5af9a46075a4958c72ba2d
unknown <lucasmontero.wed.odin@gmail.com>
2022-07-22 17:25:24 -0300

    reacomodamiento del index, add scripts buil and start in package.json

---
+++ api/src/index.js
@@ -0,0 +1,20 @@
+import app from './app'
+import sequelize from './db.js';
+import './asociations'
+import createRoles from './utils/roles';
+// import mockData from './mock/mock'
+import dotenv from 'dotenv'
+dotenv.config()
+
+import './models/user.js'
+import './models/mesage.js'
+
+const {PORT} = process.env
+
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT)
+    console.log("server listen on port", PORT)
+    createRoles()
+    // mockData()
+})
