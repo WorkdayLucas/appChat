2962e692582d92a214ca8effcc2a45c7eae8ab09
Lucas Montero <94916895+WorkdayLucas@users.noreply.github.com>
2022-07-23 03:37:18 -0300

    Update room.js

--- api/src/utils/room.js
+++ api/src/utils/room.js
@@ -57,7 +57,7 @@ export async function findRoomFromDB(roomName, userName) {
                 id: search1.id,
                 name: roomName,
                 users: search1.users,
-                mesages: [...search1.mesages]
+                mesages: [...search1.mesages].reverser()
             }
         }
 
@@ -79,7 +79,7 @@ export async function findRoomFromDB(roomName, userName) {
                 id: search2.id,
                 name: roomName,
                 users: search2.users,
-                mesages: [...search2.mesages]
+                mesages: [...search2.mesages].reverser()
             }
         }
 
