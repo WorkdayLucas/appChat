9fe2ababe7aaa8081dfbe26cce2fa8fa27f1f01c
unknown <lucasmontero.wed.odin@gmail.com>
2022-08-10 09:38:57 -0300

    update msg notif when msg is checked

--- client/src/component/main/room/mesages/Mesage.jsx
+++ client/src/component/main/room/mesages/Mesage.jsx
@@ -3,16 +3,21 @@ import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { selectCurrentUser } from '../../../../features/auth/authSlice'
 import { useUpdateMesageMutation } from '../../../../features/users/roomsApiSlice'
+import { useUpdateNotificationsMutation } from '../../../../features/users/usersApiSlice'
+import { selectCurrentContact } from '../../../../features/users/utilSlice'
 import './Mesage.css'
 
 const Mesage = ({ content, userId, time, check, msgId }) => {
 
   const user = useSelector(selectCurrentUser)
   const [updateMesage] = useUpdateMesageMutation()
+  const [updateNotifications] = useUpdateNotificationsMutation()
+  const currentContact = useSelector(selectCurrentContact)
 
   useEffect(()=>{
    if(userId!==user.id && check==="0"){
       updateMesage({id: msgId,set:"check"})
+      updateNotifications({set:"check",type:1,contactId:currentContact.id})
     }
   },[])
 
